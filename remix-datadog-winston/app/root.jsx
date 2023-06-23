b827471466750738608c0681786249bfc9d83063
amillerlite <ansley257@gmail.com>
2023-06-23 07:14:59 -0400

    Update root.jsx

--- remix-datadog-winston/app/root.jsx
+++ remix-datadog-winston/app/root.jsx
@@ -24,7 +24,6 @@ export default function App() {
         userAgent: new UAParser().getResult(),
       });
       log.debug('App loaded');
-      log.info('Testing properties', { foo: 'bar' });
     }
   }, []);
   return (
